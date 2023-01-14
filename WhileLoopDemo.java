000a378155bcc0ad179bfb5ad1ecfb7e217f3a92
kanniyappan143 <119713241+kanniyappan143@users.noreply.github.com>
2023-01-14 20:26:33 +0530

    Add files via upload

---
+++ WhileLoopDemo.java
@@ -0,0 +1,27 @@
+package loopstatement;
+
+public class WhileLoopDemo
+ 	{
+	//rules : while loop is sepreate sepreate initial value and while loop condition checking then true inside of body
+	//condition when check true print i (increment,decrement)value printed 
+
+	public static void main(String[] args) 
+	{
+	 int i=1;
+	 int j=10;
+	 while(i<11)
+	 {
+		 System.out.println("i value is "+i);
+		 i++;
+	 }
+	 while(j>1)
+	 {
+		 System.out.println("j value is"+j);
+		 j--;
+	 }
+	 }
+	 }
+	 
+	
+
+
